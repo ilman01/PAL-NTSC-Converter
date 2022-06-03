7059db27d2baef3524a889b4075420d4f25ff36b
ilman01 <106542354+ilman01@users.noreply.github.com>
2022-06-03 21:02:02 +0700

    Add files via upload

---
+++ Custom_Audio/Video and Audio Combiner.bat
@@ -0,0 +1,17 @@
+@echo off
+
+set /p inputv="Enter Input Video Path File: "
+cls
+set /p inputa="Enter Input Audio Path File: "
+cls
+set /p output="Enter Output Path File: "
+cls
+
+echo Proceed?
+pause
+cls
+
+ffmpeg -i %inputv% -i %inputa% -c:v copy -map 0:v:0 -map 1:a:0 -b:a 320k %output%
+
+echo Complete!
+pause
